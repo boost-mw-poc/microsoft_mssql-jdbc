6fe985482e519162b61f741067c0cb0e1a94de19
Divang Sharma <divangsharma@microsoft.com>
2025-02-17 12:05:29 +0530

    Hanlded review comment: removed JSON type in Textual category and precision setting in parseQueryMeta

--- src/main/java/com/microsoft/sqlserver/jdbc/DataTypes.java
+++ src/main/java/com/microsoft/sqlserver/jdbc/DataTypes.java
@@ -985,7 +985,7 @@ enum JDBCType {
      * @return true if the JDBC type is textual
      */
     private final static EnumSet<Category> textualCategories = EnumSet.of(Category.CHARACTER, Category.LONG_CHARACTER,
-    Category.CLOB, Category.NCHARACTER, Category.LONG_NCHARACTER, Category.NCLOB, Category.JSON); //FIXME: JSON is textual?
+    Category.CLOB, Category.NCHARACTER, Category.LONG_NCHARACTER, Category.NCLOB);
 
     boolean isTextual() {
         return textualCategories.contains(category);
